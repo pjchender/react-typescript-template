23f45bd6b75cd498af36e8da0004b15d43ddc1dd
pjchender <pjchender@gmail.com>
2021-07-15 17:16:25 +0800

    feat: add code spliting with dynamic import (lazy + suspense)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 
+import { lazy, Suspense } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
-import DemoContext from 'views/DemoContext';
-import DemoContextAndReducerI from 'views/DemoContextAndReducerI';
-import DemoContextAndReducerII from 'views/DemoContextAndReducerII';
-import DemoContextAndReducerAsync from 'views/DemoContextAndReducerIII';
-import DemoCustomHooks from 'views/DemoCustomHooks';
-import DemoRenderProps from 'views/DemoRenderProps';
 import Homepage from 'views/Homepage';
+
+const DemoContext = lazy(() => import('views/DemoContext'));
+const DemoContextAndReducerI = lazy(() => import('views/DemoContextAndReducerI'));
+const DemoContextAndReducerII = lazy(() => import('views/DemoContextAndReducerII'));
+const DemoContextAndReducerAsync = lazy(() => import('views/DemoContextAndReducerIII'));
+const DemoCustomHooks = lazy(() => import('views/DemoCustomHooks'));
+const DemoRenderProps = lazy(() => import('views/DemoRenderProps'));
 
 const App = (): JSX.Element => {
   return (
@@ -38,35 +40,37 @@ const App = (): JSX.Element => {
         </ul>
       </nav>
 
-      <Switch>
-        <Route path="/render-props">
-          <DemoRenderProps />
-        </Route>
+      <Suspense fallback={<h2>Dynamic Loading...</h2>}>
+        <Switch>
+          <Route path="/render-props">
+            <DemoRenderProps />
+          </Route>
 
-        <Route path="/custom-hooks">
-          <DemoCustomHooks />
-        </Route>
+          <Route path="/custom-hooks">
+            <DemoCustomHooks />
+          </Route>
 
-        <Route path="/use-context">
-          <DemoContext />
-        </Route>
+          <Route path="/use-context">
+            <DemoContext />
+          </Route>
 
-        <Route path="/use-context-reducer-i">
-          <DemoContextAndReducerI />
-        </Route>
+          <Route path="/use-context-reducer-i">
+            <DemoContextAndReducerI />
+          </Route>
 
-        <Route path="/use-context-reducer-ii">
-          <DemoContextAndReducerII />
-        </Route>
+          <Route path="/use-context-reducer-ii">
+            <DemoContextAndReducerII />
+          </Route>
 
-        <Route path="/use-context-reducer-iii">
-          <DemoContextAndReducerAsync />
-        </Route>
+          <Route path="/use-context-reducer-iii">
+            <DemoContextAndReducerAsync />
+          </Route>
 
-        <Route path="/">
-          <Homepage />
-        </Route>
-      </Switch>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </Suspense>
     </div>
   );
 };
